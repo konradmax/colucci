aa2b7f3a3ae321fbc2e45dfb5216504d07f68271
Konrad Max <konradmax91@gmail.com>
2019-12-28 13:03:06 +0100

    Init project

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+// Full Documentation - https://docs.turbo360.co
+const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
+const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const router = vertex.router()
+
+
+router.get('/', (req, res) => {
+
+	const data = {
+	greeting: 'Colucci'
+	}
+
+	res.render('index', data)
+})
+
+
+
+
+module.exports = router
