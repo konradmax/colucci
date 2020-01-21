6a1996454eb1bf08a9f281705a80ffbade28f213
Konrad Max <konradmax91@gmail.com>
2020-01-21 19:30:50 +0100

    dla Tomanka :D

--- public/js/main.js
+++ public/js/main.js
@@ -147,7 +147,7 @@
 
 	$(document).on("scroll", function() {
 
-        if($(document).scrollTop()>100) {
+        if($(document).scrollTop()>0) {
             $("header").removeClass("wrap_header").addClass("wrap_header1");
         } else {
             $("header").removeClass("wrap_header1").addClass("wrap_header");
